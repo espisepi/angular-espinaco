6e35bad6648fcfb736295b1cd8c027abd7f1860c
espisepi <sepinaco@gmail.com>
2025-03-30 14:15:27 +0200

    created example page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { ExamplePageComponent } from './pages/example-page/example-page.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'home', component: HomePageComponent },
   { path: 'about', component: AboutPageComponent },
+  { path: 'example', component: ExamplePageComponent },
 ];
